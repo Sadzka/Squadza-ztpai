787ca8e278e953d243721640fbf34e36c4a9b28e
Sadzka <sirsadzka@gmail.com>
2021-04-27 22:39:16 +0200

    Update ArticleCommentLikeRepository.php

--- src/Repository/ArticleCommentLikeRepository.php
+++ src/Repository/ArticleCommentLikeRepository.php
@@ -1,16 +1,36 @@
 <?php
 
 namespace App\Repository;
-
+use App\Entity\ArticleComment;
 use App\Entity\ArticleCommentLike;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+
+/*
+use Doctrine\ORM\EntityRepository;
+use App\Entity\ArticleComment;
+use App\Entity\ArticleCommentLike;
+
+class ArticleCommentLikeRepository extends EntityRepository
+{
+    public function sumCommentLikes(ArticleComment $comment) : int
+    {
+        return
+            $this->getDoctrine()->getRepository(ArticleCommentLike::class)->createQueryBuilder('a')
+            ->select('SUM(a.value) as score')
+            ->andWhere('a.articleComment = :val')
+            ->setParameter('val', $comment)
+            ->getQuery()
+            ->getResult()[0]['score'];
+    }
+}*/
 
 /**
  * @method ArticleCommentLike|null find($id, $lockMode = null, $lockVersion = null)
  * @method ArticleCommentLike|null findOneBy(array $criteria, array $orderBy = null)
  * @method ArticleCommentLike[]    findAll()
  * @method ArticleCommentLike[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method int                     sumCommentLikes(ArticleComment $comment)
  */
 class ArticleCommentLikeRepository extends ServiceEntityRepository
 {
@@ -18,6 +38,19 @@ class ArticleCommentLikeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ArticleCommentLike::class);
     }
+
+    public function sumCommentLikes(ArticleComment $comment) : int
+    {
+        $result = 
+            $this->createQueryBuilder('a')
+            ->select('SUM(a.value) as score')
+            ->andWhere('a.articleComment = :val')
+            ->setParameter('val', $comment)
+            ->getQuery()
+            ->getResult()[0]['score'];
+        return $result ? $result : 0;
+    }
+
 
     // /**
     //  * @return ArticleCommentLike[] Returns an array of ArticleCommentLike objects
